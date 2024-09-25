19f5d3d418728cf44417dcd84aa4d11d3de73297
TheDutchSpartan <59475324+TheDutchSpartan@users.noreply.github.com>
2024-09-25 16:23:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 import requests
 import pandas as pd
 # import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
