90df76ef13b6ce106349b61fad8d9445296107a5
TheDutchSpartan <59475324+TheDutchSpartan@users.noreply.github.com>
2024-09-26 14:43:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -297,4 +297,4 @@ Door de volledigheid en betrouwbaarheid van de data te waarborgen, zullen de inz
 #Disclaimer
 
 st.subheader('Disclaimer')
-st.write("""Ook al bevat de dataset geen ontbrekenden waarden, zijn de provincies niet altijd accuraat. Zo zijn er EU-landen die wel provincies/regio's bevatten, maar dat niet is aangegeven in de dataset. Zo lijkt het dus alsof sommige landen geen provincies hebben terwijl dit wel het geval is."""
+st.write("""Ook al bevat de dataset geen ontbrekenden waarden, zijn de provincies niet altijd accuraat. Zo zijn er EU-landen die wel provincies/regio's bevatten, maar dat niet is aangegeven in de dataset. Zo lijkt het dus alsof sommige landen geen provincies hebben terwijl dit wel het geval is.""")
